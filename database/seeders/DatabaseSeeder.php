656b581c931b855a782a4ead832a23561cce84a4
Mihail <mmisha96@gmail.com>
2021-03-15 11:26:43 +0200

    Prepupulate users and questions tables using model factories and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create()->each(function ($user) {
+            $questions = Question::factory(mt_rand(5, 10))->make();
+            $user->questions()->saveMany($questions);
+        });
+
     }
 }
