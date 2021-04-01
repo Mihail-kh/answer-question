abe0a7278cf0b6c56db2b4afeb8d2c318b42c955
Mihail <mmisha96@gmail.com>
2021-04-01 11:17:56 +0300

    Implement accept the answer as best answer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AcceptAnswerController;
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
@@ -27,4 +28,6 @@ Route::resource('questions', QuestionController::class)->except('show');
 //Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
 Route::resource('questions.answers', AnswerController::class)->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', [QuestionController::class, 'show'])->name('questions.show');
+
+Route::post('/answers/{answer}/accept', AcceptAnswerController::class)->name('answers.accept');
 
